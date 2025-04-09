be4ea4f9e1966e22390ad343496dd9b70a981256
TahseenNoor <118875293+TahseenNoor@users.noreply.github.com>
2025-04-09 23:04:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import joblib
 
 # Load your dataset and model
 df = pd.read_csv("Cleaned_Autodock_Results.csv")
-model = joblib.load("binding_model_train.pkl")
+model = joblib.load("model.pkl")
+
 
 
 # App title
